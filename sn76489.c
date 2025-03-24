8f4d6557ff0f783cc7f812cf4f7a9e24ac63be0e
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2025-03-24 19:34:14 +0000

    update code from totalsms

--- sn76489.c
+++ sn76489.c
@@ -21,14 +21,13 @@ enum ChannelType {
 };
 
 enum LatchType {
-    LatchType_TONE = 0,
-    LatchType_NOISE = 0,
-    LatchType_VOL = 1,
+    LatchType_CHANNEL = 0,
+    LatchType_VOLUME = 1,
 };
 
-enum {
-    WHITE_NOISE = 1,
-    PERIDOIC_NOISE = 0,
+enum NoiseMode {
+    NoiseMode_PERIDOIC = 0,
+    NoiseMode_WHITE = 1,
 };
 
 struct Sn76489Tone
@@ -87,25 +86,43 @@ static const int16_t VOLUME_TABLE[16] = {
     16383, 13014, 10337, 8211, 6522, 5181, 4115, 3284, 2596, 2062, 1638, 1301, 1033, 821, 652, 0
 };
 
-static const uint16_t NOISE_CHANNEL_FREQ[3] = {
-    16*16, 32*16, 64*16
-};
-
 #define psg_min(x, y) (x) < (y) ? (x) : (y)
 #define psg_max(x, y) (x) > (y) ? (x) : (y)
 #define psg_clamp(a, x, y) psg_max(psg_min(a, y), x)
 #define psg_array_size(a) (sizeof(a) / sizeof(a[0]))
+
+static inline void add_delta_internal(Sn76489* psg, struct Sn76489Channel* c, void(*func)(blip_wrap_t*, unsigned,int,int), unsigned clock_time, int sample, unsigned lr)
+{
+    const int delta = sample - c->amp[lr];
+    if (delta) // same as (sample != amp)
+    {
+        func(psg->blip, clock_time, delta, lr);
+        c->amp[lr] += delta; // same as (amp = sample)
+    }
+}
+
+static inline void add_delta(Sn76489* psg, struct Sn76489Channel* c, unsigned clock_time, int left, int right)
+{
+    add_delta_internal(psg, c, blip_wrap_add_delta, clock_time, left, 0);
+    add_delta_internal(psg, c, blip_wrap_add_delta, clock_time, right, 1);
+}
+
+static inline void add_delta_fast(Sn76489* psg, struct Sn76489Channel* c, unsigned clock_time, int left, int right)
+{
+    add_delta_internal(psg, c, blip_wrap_add_delta_fast, clock_time, left, 0);
+    add_delta_internal(psg, c, blip_wrap_add_delta_fast, clock_time, right, 1);
+}
 
 static inline bool parity(unsigned value)
 {
     #if defined(__has_builtin) && __has_builtin(__builtin_parity)
         return !__builtin_parity(value);
     #else
-        // SOURCE: https://graphics.stanford.edu/~seander/bithacks.html#ParityParallel
-        value ^= value >> 8; // 16-bit
-        value ^= value >> 4; // 8-bit
-        value &= 0xF;
-        return !((0x6996 >> value) & 0x1);
+        value ^= value>>8;
+        value ^= value>>4;
+        value ^= value>>2;
+        value ^= value>>1;
+        return value & 1;
     #endif
 }
 
@@ -117,31 +134,11 @@ static inline unsigned channel_get_freq(const Sn76489* psg, int num)
         {
             return psg->tone[2].tone * PSG_CLOCK_DIVIDER;
         }
-        return NOISE_CHANNEL_FREQ[psg->noise.shift_rate];
+        return (16 << psg->noise.shift_rate) * PSG_CLOCK_DIVIDER;
     }
     else
     {
         return psg->tone[num].tone * PSG_CLOCK_DIVIDER;
-    }
-}
-
-static inline void add_delta(Sn76489* psg, struct Sn76489Channel* c, unsigned clock_time, int sample, unsigned lr)
-{
-    const int delta = sample - c->amp[lr];
-    if (delta) // same as (sample != amp)
-    {
-        blip_wrap_add_delta(psg->blip, clock_time, delta, lr);
-        c->amp[lr] += delta; // same as (amp = sample)
-    }
-}
-
-static inline void add_delta_fast(Sn76489* psg, struct Sn76489Channel* c, unsigned clock_time, int sample, unsigned lr)
-{
-    const int delta = sample - c->amp[lr];
-    if (delta) // same as (sample != amp)
-    {
-        blip_wrap_add_delta_fast(psg->blip, clock_time, delta, lr);
-        c->amp[lr] += delta; // same as (amp = sample)
     }
 }
 
@@ -162,7 +159,7 @@ static void channel_sync_psg(Sn76489* psg, unsigned num, unsigned time)
     // save new timestamp
     c->timestamp = new_timestamp;
 
-    // already clocked on this cycle, or bad timestamp
+    // already clocked on this cycle, or bad timestamp.
     if (until <= 0)
     {
         return;
@@ -174,14 +171,17 @@ static void channel_sync_psg(Sn76489* psg, unsigned num, unsigned time)
         from = base_clock + until;
     }
 
+    const unsigned freq = channel_get_freq(psg, num);
+    if (!freq)
+    {
+        return;
+    }
+
     const int sample = blip_apply_volume_to_sample(psg->blip, VOLUME_TABLE[c->volume], psg->channel_volume[num]);
-    // const int sample = VOLUME_TABLE[c->volume];
     const int sample_left = sample * c->enabled[0];
     const int sample_right = sample * c->enabled[1];
 
     /*
-        from the docs:
-
         Sample playback makes use of a feature of the psg's tone generators:
         when the half-wavelength (tone value) is set to 1, they output a DC offset
         value corresponding to the volume level (i.e. the wave does not flip-flop).
@@ -190,63 +190,49 @@ static void channel_sync_psg(Sn76489* psg, unsigned num, unsigned time)
         This effect is used by the sega intro in Tail's Adventure and
         sonic tripple trouble.
     */
-    const unsigned freq = channel_get_freq(psg, num);
-    if (freq <= PSG_CLOCK_DIVIDER)
+    if (freq == PSG_CLOCK_DIVIDER)
     {
         const int left = +sample_left;
         const int right = +sample_right;
-        add_delta_fast(psg, c, from, left, 0);
-        add_delta_fast(psg, c, from, right, 1);
+        add_delta_fast(psg, c, from, left, right);
         return;
     }
 
     int left = (c->polarity ? +sample_left : -sample_left);
     int right = (c->polarity ? +sample_right : -sample_right);
-    add_delta(psg, c, from, left, 0);
-    add_delta(psg, c, from, right, 1);
-
+    add_delta(psg, c, from, left, right);
     c->frequency_timer -= until;
 
-    if (num == ChannelType_NOISE)
+    while (c->frequency_timer <= 0)
     {
-        const unsigned tapped_bits = psg->lfsr_tapped_bit;
-        const unsigned feedback_bit = psg->lfsr_feed_bit;
-
-        while (c->frequency_timer <= 0)
+        const bool old_polarity = c->polarity;
+        if (num == ChannelType_NOISE)
         {
             psg->noise.flip_flop ^= 1;
             if (psg->noise.flip_flop)
             {
                 psg->noise.lfsr = (psg->noise.lfsr >> 1) |
-                    ((psg->noise.mode == WHITE_NOISE
-                    ? parity(psg->noise.lfsr & tapped_bits)
-                    : psg->noise.lfsr & 1) << feedback_bit);
+                    ((psg->noise.mode == NoiseMode_WHITE
+                    ? parity(psg->noise.lfsr & psg->lfsr_tapped_bit)
+                    : psg->noise.lfsr & 1) << psg->lfsr_feed_bit);
 
-                const unsigned new_polarity = psg->noise.lfsr & 0x1;
-                if (c->polarity != new_polarity)
-                {
-                    c->polarity = new_polarity;
-                    left = -left;
-                    right = -right;
-                    add_delta_fast(psg, c, from, left, 0);
-                    add_delta_fast(psg, c, from, right, 1);
-                }
+                c->polarity = psg->noise.lfsr & 0x1;
             }
-
-            c->frequency_timer += freq;
-            from += freq;
         }
-    }
-    else // tone
-    {
-        while (c->frequency_timer <= 0)
+        else
         {
             c->polarity ^= 1;
-            add_delta(psg, c, from, left, 0);
-            add_delta(psg, c, from, right, 1);
-            c->frequency_timer += freq;
-            from += freq;
         }
+
+        if (c->polarity != old_polarity && (left || right))
+        {
+            left = -left;
+            right = -right;
+            add_delta(psg, c, from, left, right);
+        }
+
+        c->frequency_timer += freq;
+        from += freq;
     }
 }
 
@@ -261,7 +247,7 @@ static void channel_sync_psg_all(Sn76489* psg, unsigned time)
 static void noise_channel_reset(Sn76489* psg, unsigned data)
 {
     psg->noise.lfsr = 1 << psg->lfsr_feed_bit;
-    psg->noise.flip_flop = true; // todo: confirm flipflop is reset
+    psg->noise.flip_flop = true;
     psg->noise.shift_rate = data & 0x3;
     psg->noise.mode = (data >> 2) & 0x1;
 }
@@ -275,22 +261,20 @@ static void latch_reg_write(Sn76489* psg, unsigned value, unsigned time)
     // sync the newly latched channel as the data is about to be changed
     channel_sync_psg(psg, psg->latched_channel, time);
 
-    if (psg->latched_type == LatchType_VOL)
+    if (psg->latched_type == LatchType_VOLUME)
     {
         psg->channels[psg->latched_channel].volume = data & 0xF;
     }
     else
     {
-        switch (psg->latched_channel)
+        if (psg->latched_channel == ChannelType_NOISE)
         {
-            case 0: case 1: case 2:
-                psg->tone[psg->latched_channel].tone &= 0x3F0;
-                psg->tone[psg->latched_channel].tone |= data;
-                break;
-
-            case 3:
-                noise_channel_reset(psg, data);
-                break;
+            noise_channel_reset(psg, data);
+        }
+        else
+        {
+            psg->tone[psg->latched_channel].tone &= 0x3F0;
+            psg->tone[psg->latched_channel].tone |= data;
         }
     }
 }
@@ -300,22 +284,20 @@ static void data_reg_write(Sn76489* psg, unsigned value, unsigned time)
     const unsigned data = value & 0x3F;
     channel_sync_psg(psg, psg->latched_channel, time);
 
-    if (psg->latched_type == LatchType_VOL)
+    if (psg->latched_type == LatchType_VOLUME)
     {
         psg->channels[psg->latched_channel].volume = data & 0xF;
     }
     else
     {
-        switch (psg->latched_channel)
+        if (psg->latched_channel == ChannelType_NOISE)
         {
-            case 0: case 1: case 2:
-                psg->tone[psg->latched_channel].tone &= 0xF;
-                psg->tone[psg->latched_channel].tone |= data << 4;
-                break;
-
-            case 3:
-                noise_channel_reset(psg, data);
-                break;
+            noise_channel_reset(psg, data);
+        }
+        else
+        {
+            psg->tone[psg->latched_channel].tone &= 0xF;
+            psg->tone[psg->latched_channel].tone |= data << 4;
         }
     }
 }
@@ -357,7 +339,6 @@ void psg_quit(Sn76489* psg)
         if (psg->blip)
         {
             blip_wrap_delete(psg->blip);
-            psg->blip = NULL;
         }
         free(psg);
     }
@@ -366,9 +347,16 @@ void psg_quit(Sn76489* psg)
 void psg_reset(Sn76489* psg, unsigned lfsr_tapped_bit, unsigned lfsr_feed_bit)
 {
     psg_clear_samples(psg);
+
+    psg->lfsr_tapped_bit = lfsr_tapped_bit;
+    psg->lfsr_feed_bit = lfsr_feed_bit;
     memset(&psg->channels, 0, sizeof(psg->channels));
     memset(&psg->tone, 0, sizeof(psg->tone));
     memset(&psg->noise, 0, sizeof(psg->noise));
+    noise_channel_reset(psg, 0);
+    psg->latched_channel = 0;
+    psg->latched_type = 0;
+
     for (unsigned i = 0; i < psg_array_size(psg->channels); i++)
     {
         psg->channels[i].polarity = 1;
@@ -376,10 +364,6 @@ void psg_reset(Sn76489* psg, unsigned lfsr_tapped_bit, unsigned lfsr_feed_bit)
         psg->channels[i].enabled[0] = true;
         psg->channels[i].enabled[1] = true;
     }
-    psg->lfsr_tapped_bit = lfsr_tapped_bit;
-    psg->lfsr_feed_bit = lfsr_feed_bit;
-    noise_channel_reset(psg, 0);
-    psg->latched_channel = 0;
 }
 
 void psg_write_io(Sn76489* psg, unsigned value, unsigned time)
@@ -500,7 +484,7 @@ unsigned psg_state_size(void)
 
 int psg_save_state(const Sn76489* psg, void* data, unsigned size)
 {
-    if (!data || size < psg_state_size())
+    if (!psg || !data || size < psg_state_size())
     {
         return 1;
     }
@@ -510,7 +494,7 @@ int psg_save_state(const Sn76489* psg, void* data, unsigned size)
 
 int psg_load_state(Sn76489* psg, const void* data, unsigned size)
 {
-    if (!data || size < psg_state_size())
+    if (!psg || !data || size < psg_state_size())
     {
         return 1;
     }
