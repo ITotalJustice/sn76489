146172f772b022980defd9dcb117a018a828da94
ITotalJustice <47043333+ITotalJustice@users.noreply.github.com>
2025-03-24 20:14:55 +0000

    forgot to upload the latest header from the last commit

--- sn76489.h
+++ sn76489.h
@@ -6,15 +6,15 @@ extern "C" {
 #endif
 
 enum Sn76489LfsrTappedBit {
-    Sn76489LfsrTappedBit_SMS = 0x0009, // sms, gg
-    Sn76489LfsrTappedBit_GG = 0x0009, // sms, gg
-    Sn76489LfsrTappedBit_SG = 14, // sg100
+    Sn76489LfsrTappedBit_SMS = 0x0009,   /* SMS1, SMS2, Genesis and Game Gear. */
+    Sn76489LfsrTappedBit_SG = 0x0003,    /* SG-1000, OMV, SC-3000H, BBC Micro and Colecovision. */
+    Sn76489LfsrTappedBit_TANDY = 0x0011, /* Tandy 1000. */
 };
 
 enum Sn76489LfsrFeedBit {
-    Sn76489LfsrFeedBit_SMS = 15, // sms, gg
-    Sn76489LfsrFeedBit_GG = 15, // sms, gg
-    Sn76489LfsrFeedBit_SG = 14, // sg100
+    Sn76489LfsrFeedBit_SMS = 15,   /* SMS1, SMS2, Genesis and Game Gear. */
+    Sn76489LfsrFeedBit_SG = 14,    /* SG-1000, OMV, SC-3000H, BBC Micro and Colecovision. */
+    Sn76489LfsrFeedBit_TANDY = 14, /* Tandy 1000. */
 };
 
 typedef struct Sn76489 Sn76489;
@@ -22,7 +22,7 @@ typedef struct Sn76489 Sn76489;
 /* ------------------------- */
 /* ------Initialise Api----- */
 /* ------------------------- */
-/* ensure you call this at start-up .*/
+/* ensure you call this at start-up. */
 Sn76489* psg_init(double clock_rate, double sample_rate);
 /* call to free allocated memory by blip buf. */
 void psg_quit(Sn76489*);
@@ -30,7 +30,7 @@ void psg_quit(Sn76489*);
 void psg_reset(Sn76489*, unsigned lfsr_tapped_bit, unsigned lfsr_feed_bit);
 
 /* ------------------------- */
-/* ------Input Api------ */
+/* --------Input Api-------- */
 /* ------------------------- */
 /* writes to an io register. */
 void psg_write_io(Sn76489*, unsigned value, unsigned time);
@@ -52,15 +52,6 @@ void psg_set_treble(Sn76489*, double treble_db);
 void psg_update_timestamp(Sn76489*, int time);
 
 /* ------------------------- */
-/* ------Advanced Api------- */
-/* ------------------------- */
-/* returns value of io register, without unused bits applied / masked, */
-/* regardless if the apu is enabled or not. */
-/* useful for gui io viewer. */
-unsigned psg_read_io_raw(const Sn76489*, unsigned addr);
-unsigned psg_agb_read_io_raw(const Sn76489*, unsigned addr);
-
-/* ------------------------- */
 /* ------Sample Output------ */
 /* ------------------------- */
 /* returns how many cycles are needed until sample_count == psg_samples_avaliable() */
@@ -73,6 +64,16 @@ void psg_end_frame(Sn76489*, unsigned time);
 int psg_read_samples(Sn76489*, short out[], int count);
 /* removes all samples. */
 void psg_clear_samples(Sn76489*);
+
+/* ------------------------- */
+/* ------SaveState Api------ */
+/* ------------------------- */
+/* returns the size needed for savestates. */
+unsigned psg_state_size(void);
+/* creates a savestate, returns 0 on success. */
+int psg_save_state(const Sn76489*, void* data, unsigned size);
+/* loads a savestate, returns 0 on success. */
+int psg_load_state(Sn76489*, const void* data, unsigned size);
 
 #ifdef __cplusplus
 }
